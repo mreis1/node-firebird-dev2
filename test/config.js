5addceadf535fa40c2642172b8e02632911f21ea
Marcio (PRO) <marcio.reis1@hotmail.com>
2024-07-17 11:57:51 +0200

    adds env variables to provide better control options over tests  environment

--- test/config.js
+++ test/config.js
@@ -6,21 +6,21 @@ const dbName = 'test-' + currentDate.getTime() + '.fdb';
 
 exports.default = {
     database: path.join(process.env.FIREBIRD_DATA || testDir, dbName),
-    host: '127.0.0.1',
-    port: 3050,
-    user: 'SYSDBA',
-    password: 'masterkey',
+    host: process.env.FIREBIRD_HOST || '127.0.0.1',
+    port: process.env.FIREBIRD_PORT || 3050,
+    user: process.env.FIREBIRD_USER || 'SYSDBA',
+    password: process.env.FIREBIRD_PW || 'masterkey',
     role: null,
     pageSize: 4096,
     timeout: 3000,
     lowercase_keys: true,
     retryConnectionInterval: 100
 };
-
-exports.currentDate = currentDate;
-exports.testDir = testDir;
-
-exports.extends = function(base, args) {
-    return Object.assign({}, base, args);
-}
-
+console.log(exports.default)
+// exports.currentDate = currentDate;
+// exports.testDir = testDir;
+//
+// exports.extends = function(base, args) {
+//     return Object.assign({}, base, args);
+// }
+//
