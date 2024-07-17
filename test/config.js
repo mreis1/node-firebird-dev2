351b767bf88968dd009f6766f0001cb8ae1d7899
Marcio Reis (Win10) <marcio@heitzsystem.com>
2024-07-17 17:27:25 +0200

    update the test configuration to support more environment variables for better test control

--- test/config.js
+++ test/config.js
@@ -5,18 +5,18 @@ const testDir = path.resolve(__dirname);
 const dbName = 'test-' + currentDate.getTime() + '.fdb';
 
 exports.default = {
-    database: path.join(process.env.FIREBIRD_DATA || testDir, dbName),
-    host: '127.0.0.1',
-    port: 3050,
-    user: 'SYSDBA',
-    password: 'masterkey',
+    database: process.env.FIREBIRD_DB ? process.env.FIREBIRD_DB.replace(new RegExp(/<TS>/,'gi'), Date.now()) : path.join(process.env.FIREBIRD_DATA || testDir, dbName),
+    databaseGds: process.env.FIREBIRD_DB_GDS ? process.env.FIREBIRD_DB.replace(new RegExp(/<TS>/,'gi'), Date.now()) : path.join(process.env.FIREBIRD_DATA || testDir, dbName),
+    host: process.env.FIREBIRD_HOST || '127.0.0.1',
+    port: process.env.FIREBIRD_PORT || 3050,
+    user: process.env.FIREBIRD_USER || 'SYSDBA',
+    password: process.env.FIREBIRD_PW || 'masterkey',
     role: null,
     pageSize: 4096,
     timeout: 3000,
     lowercase_keys: true,
     retryConnectionInterval: 100
 };
-
 exports.currentDate = currentDate;
 exports.testDir = testDir;
 
